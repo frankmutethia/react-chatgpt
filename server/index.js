f0d3bba629c699815e804063be9858c38c4b98c3
FrankMutethia <frankmutethia81@gmail.com>
2024-08-14 00:27:09 +0300

    creating a connection with openai api and scroll functionality

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const socketServer = require('./src/socketServer');
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const socketServer = require("./src/socketServer");
+
+const { connectWithOpenAIApi } = require("./src/ai");
+connectWithOpenAIApi();
+
 const app = express();
 
 const server = http.createServer(app);
@@ -9,12 +13,12 @@ socketServer.registerSocketServer(server);
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello server is working perfectly');
+app.get("/", (req, res) => {
+  res.send("Hello server is working");
 });
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, ()=> {
-    console.log(`App started listening at port ${PORT}`);
-})
+server.listen(PORT, () => {
+  console.log(`App started listening at port ${PORT}`);
+});
--- server/src/socketServer.js
+++ server/src/socketServer.js
@@ -1,5 +1,6 @@
 const { Server } = require("socket.io");
 const { v4: uuid } = require("uuid");
+const { getOpenai } = require("./ai");
 
 let sessions = {};
 
@@ -52,12 +53,41 @@ const sessionHistoryHandler = (socket, data) => {
   }
 };
 
-const conversationMessageHandler = (socket, data) => {
+const conversationMessageHandler = async (socket, data) => {
   const { sessionId, message, conversationId } = data;
 
+  const openai = getOpenai();
+
+  const previousConversationMessages = [];
+
   if (sessions[sessionId]) {
+    const existingConversation = sessions[sessionId].find(
+      (c) => c.id === conversationId
+    );
+
+    if (existingConversation) {
+      previousConversationMessages.push(
+        ...existingConversation.messages.map((m) => ({
+          content: m.content,
+          role: m.aiMessage ? "assistant" : "user",
+        }))
+      );
+    }
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        ...previousConversationMessages,
+        { role: "user", content: message.content },
+      ],
+    });
+
+    const aiMessageContent = response?.data?.choices[0]?.message?.content;
+
     const aiMessage = {
-      content: "Hello here is AI",
+      content: aiMessageContent
+        ? aiMessageContent
+        : "Error occurred when trying to get message from the AI",
       id: uuid(),
       aiMessage: true,
     };
@@ -94,95 +124,3 @@ const conversationDeleteHandler = (_, data) => {
 };
 
 module.exports = { registerSocketServer };
-
-
-
-// const {Server} = require('socket.io');//fetching the server from socket.io
-// const {v4: uuid} = require('uuid');
-
-// let sessions = {};
-
-
-// const registerSocketServer = (server) =>{
-// const io = new Server(server,{
-//     cors:{
-//         origin:"*",
-//         methods:["GET", "POST"],
-//     }
-// });
-// io.on("connection", (socket)=> {
-//     console.log(`User Connected ${socket.id}`);
-     
-//     socket.on("session-history", (data) => {
-//     sessionHistoryHandler(socket, data);
-//     });
-
-//     socket.on("conversation-message", (data) => {
-//     conversationMessageHandler(socket, data);
-//     })
-//     });
-// }
-
-
-// const sessionHistoryHandler = (socket, data) => {
-// const { sessionId } = data;
-
-// if(sessions[sessionId]){
-//   //send  existing session data back to user
-//   socket.emit("session-details", {
-//     sessionId,
-//     conversations: sessions[sessionsId],
-//   });
-// } else{
-//     const newSessionId = uuid();
-
-//     sessions[newSessionId] = []
-//     //define session details that will be sent back to the user
-//     const sessionDetails = {
-//     sessionId: newSessionId,
-//     conversations: [],
-//     };
-
-//     socket.emit("session-details", sessionDetails);
-// }
-
-// }
-
-
-// const conversationMessageHandler = (socket, data) =>{
-//     console.log("message came from client side");
-// console.log(data);
-// const {sessionid, message, conversationId } = data;
-// if (sessions[sessionId]) {
-//     const aiMessage = {
-//         content: "Hello here is AI",
-//         id: uuid(),
-//         aiMessage: true,
-//       };
-  
-//       const conversation = sessions[sessionId].find(
-//         (c) => c.id === conversationId
-//       );
-// //if conversation does not exist
-// if(!conversation){
-//     sessions[sessionId].push({//new conversation has been created here
-//     id: conversationId,
-//     messages: [message, aiMessage],
-//     });
-// }
-
-
-// //if conversation does exist
-// if(!conversation){
-// //messages are the array, we are also pushing this message onto the array
-// conversation.messages.push(message,aiMessage);
-// }
-
-// const updatedConversation = sessions[sessionId].find
-// (c => c.id === conversationId
-// );
-
-// socket.emit("conversation-details", updatedConversation);
-// }
-// };
-// module.exports = {registerSocketServer};
